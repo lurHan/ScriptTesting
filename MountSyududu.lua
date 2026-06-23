f03511aa37c099603f278459149cc0318c9fea55
Riz <rizkiem36@gmail.com>
2026-06-23 19:10:38 +0700

    y

--- MountSyududu.lua
+++ MountSyududu.lua
@@ -2,7 +2,7 @@ local Players = game:GetService("Players")
 local player = Players.LocalPlayer.Character.HumanoidRootPart
 
 local location = CFrame.new(244.37, 53.9, 25.97)
-local humanoid = game.Players.LocalPlayer.Character.Humanoid
-humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
-wait(0.1)
+-- local humanoid = game.Players.LocalPlayer.Character.Humanoid
+-- humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+-- wait(0.1)
 player.CFrame = location
